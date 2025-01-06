548eea0608713d8d0851efa2e29949fe8ad9f3c6
emachizy <emachi2011@gmail.com>
2025-01-06 08:26:52 -0800

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
 
-module.exports = withMT({
+export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
-        scrolling: "scrolling 30s linear infinite", // Define the animation
+        scrolling: "scrolling 30s linear infinite",
       },
       keyframes: {
         scrolling: {
-          "0%": { transform: "translateX(100%)" }, // Start off the screen to the right
-          "100%": { transform: "translateX(-100%)" }, // End off the screen to the left
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
     },
